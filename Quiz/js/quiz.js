0ed1afee14b36ef7e820516076440b8169162d2b
Aaron <hobogod3@gmail.com>
2020-05-07 15:13:39 -0400

    Rough draft quiz

--- Quiz/js/quiz.js
+++ Quiz/js/quiz.js
@@ -1,34 +1,39 @@
 const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
+const submitButton = document.getElementById('submitButton');
 const myQuestions = [
     [
         question = "Who's that pokemon!?",
-        answers = {
-            a: "Pideler",
-            b: "Bulbeler",
-            c: "Caterem"
-        },
+        answers = [
+            a = "Pideler",
+            b = "Bulbeler",
+            c = "Caterem"
+        ],
         correctAnswer = "a"
     ],
     [
         question = "Who's that pokemon!?",
-        answers = {
-            a: "Caterpuff",
-            b: "Catertata",
-            c: "Caterow"
-        },
+        answers = [
+            a = "Caterpuff",
+            b = "Catertata",
+            c = "Caterow"
+        ],
         correctAnswer = "c"
     ],
     [
         question = "Who's that pokemon!?",
-        answers = {
-            a: "Snortle",
-            b: "Snorb",
-            c: "Snoreotto"
-        },
+        answers = [
+            a = "Snortle",
+            b = "Snorb",
+            c = "Snoreotto"
+        ],
         correctAnswer = "b"
     ]
 ];
+var questionNumber = 0;
+var questionSet = "pokemon";
+var results = 0;
+var image_src;
+var image;
 
 function buildQuiz() {
     const quizContainer = document.getElementById('quiz');
@@ -36,8 +41,12 @@ function buildQuiz() {
     nextButton = document.getElementById('next');
     nextButton.addEventListener('click', nextQuestion);
 
-
     /*
+        console.log(myQuestions);
+        console.log(myQuestions[0][2]);
+        console.log(myQuestions[0][1][0]);
+        console.log(myQuestions[1][1]);
+
     for (var i = 0; i < myQuestions.length; i++) {
         var title = document.createElement("h2");
         var title_text = document.createTextNode(myQuestions[i][0]);
@@ -57,10 +66,57 @@ function buildQuiz() {
 }
 
 function nextQuestion() {
-    console.log("Next Question");
+
+    if (questionNumber > myQuestions.length -1)
+        return;
+
+    console.log("Next Question");        
+    var radio_buttons = document.getElementsByName('question1');
+    for (var i = 0; i < radio_buttons.length; i++) {
+        console.log(radio_buttons[i].checked);
+        if (radio_buttons[i].value == myQuestions[questionNumber][2] && radio_buttons[i].checked)
+            results++;
+        radio_buttons[i].checked = false;
+    }
+
+    questionNumber++;
+
+    if (questionNumber >= myQuestions.length) {
+        showResults();
+        return;
+    }
+
+    image_src = "../img/" + questionSet + "/image" + (questionNumber + 1) + ".png";
+
+    image = document.getElementById('quiz-image');
+    image.src = image_src;
+
+    var label = document.getElementById('labela');
+    label.textContent = myQuestions[questionNumber][1][0];
+    label = document.getElementById('labelb');
+    label.textContent = myQuestions[questionNumber][1][1];
+    label = document.getElementById('labelc');
+    label.textContent = myQuestions[questionNumber][1][2];
+    
+
 }
     
-    
+function showResults() {
+    image_src = "../img/" + questionSet + "/results.png";
+    image.src = image_src;
+    if (results == 0)
+        console.log('You suck');
+    else {
+        console.log("You got " + results + " out of " + myQuestions.length + " correct!")
+    }
+    var resultDiv = document.getElementById('quiz-question');
+    while (resultDiv.hasChildNodes()) {
+        resultDiv.removeChild(resultDiv.firstChild);
+    }
+    var button = document.getElementById('next');
+    button.parentNode.removeChild(button);
+
+}
     
     
 /*    const output = [];
